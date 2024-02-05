1312a688af40de4a62ee312b34404b748a2034ee
yunxi.zhang <yunxi.zhang@amam64xwppqxj.broadband>
2024-02-05 12:45:23 +0000

    update runnode scripts

--- QBFT-Network/runNode1.sh
+++ QBFT-Network/runNode1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-besu --data-path=./Node-1/data --genesis-file=./genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,QBFT --host-allowlist="*" --rpc-http-cors-origins="all"
+besu --data-path=./Node-1/data --genesis-file=./genesis.json --rpc-http-enabled --rpc-http-api=ETH,NET,QBFT,WEB3 --host-allowlist="*" --rpc-http-cors-origins="all" --privacy-enabled --privacy-url=http://127.0.0.1:9102 --privacy-public-key-file=./Node-1/Tessera/nodeKey.pub --min-gas-price=0
