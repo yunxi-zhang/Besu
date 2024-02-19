03f70bba0657fd675538eadddd27f9546b94e530
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-02-19 13:45:17 +0000

    add a new contractLogs folder and a tempalte log file

--- node/scripts/deploy_upgradeable_contract.js
+++ node/scripts/deploy_upgradeable_contract.js
@@ -1,4 +1,6 @@
 const { ethers, upgrades } = require("hardhat");
+const fs = require("fs");
+const path = require('path');
 
 async function main() {
   const Box = await ethers.getContractFactory("Box");
@@ -6,6 +8,9 @@ async function main() {
   const box = await upgrades.deployProxy(Box, [45], { initializer: 'store' });
   await box.waitForDeployment();
   console.log('Box deployed to:', box.target);
+  const logDir = path.join(__dirname, "../contractLogs/");
+  const writer = fs.createWriteStream(logDir + '/deployContractLog.json');
+  writer.write('{"contract":"Box", "address":"' + box.target + '", "timeStamp":"' + Date.now() + '", "humanReadableTimeStamp":"' + new Date(Date.now() * 1000) + '"}')
 }
 
 main();
