cb9af031602b4e61d5836fd9f2340683eb26cbd2
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-02-20 16:44:42 +0000

    add readme on key gen and update readme

--- QBFT-Network/admin/genKeyPair.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-besu --data-path=. public-key export-address --to=key.address
-besu --data-path=. public-key export --to=key.pub
---
+++ QBFT-Network/genKeyPair.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Generate key pairs for admin
+# besu --data-path=./admin public-key export-address --to=./admin/key.address
+# besu --data-path=./admin public-key export --to=./admin/key.pub
+# Generate key pairs for user1
+# besu --data-path=./user1 public-key export-address --to=./user1/key.address
+# besu --data-path=./user1 public-key export --to=./user1/key.pub
+# Generate key pairs for user2
+# besu --data-path=./user2 public-key export-address --to=./user2/key.address
+# besu --data-path=./user2 public-key export --to=./user2/key.pub
