ac9d846427c6d5d7f8395eea335da746bfa25d2e
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-02-28 10:52:30 +0000

    update box contract and its unit test

--- node/contracts/Box.sol
+++ node/contracts/Box.sol
@@ -1,44 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-// Import Ownable from the OpenZeppelin Contracts library
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-// Make Box inherit from the Ownable contract
-contract Box {
+contract Box is OwnableUpgradeable {
     uint256 private _value;
-    Auth private _auth;
-    uint256 public x;
-    bool private initialized;
 
-    // Emitted when the stored value changes
-    event ValueChanged(uint256 value);
+    event ValueChanged(address indexed setter, uint256 newValue);
 
-    function initialize() public {
-        require(!initialized, "Contract instance has already been initialized");
-        initialized = true;
+    function initialize(address initialOwner) initializer public{
+        __Ownable_init(initialOwner);
     }
 
-    // Stores a new value in the contract
     function store(uint256 value) public {
-        // Require that the caller is registered as an administrator in Auth
-        // require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
         _value = value;
-        emit ValueChanged(value);
+        emit ValueChanged(msg.sender, value);
     }
 
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
+    function retrieve() external view onlyOwner returns(uint256) {
         return _value;
-    }
-
-    // Increments the stored value by 1
-    function increment() public {
-        _value = _value + 1;
-        emit ValueChanged(_value);
     }
 }
--- node/test/Box.test.js
+++ node/test/Box.test.js
@@ -1,85 +1,54 @@
-// test/Box.test.js
-// Load dependencies
 const { expect } = require("chai");
-const hre = require("hardhat");
-const { Contract, Wallet } = require("ethers");
-const path = require("path");
-const fs = require("fs").promises;
-const url = "http://127.0.0.1:8545";
+const { ethers, network } = require("hardhat");
 
 // Start test block
 describe("Box", function () {
-  const abi = [
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "value",
-          type: "uint256",
-        },
-      ],
-      name: "ValueChanged",
-      type: "event",
-    },
-    {
-      inputs: [],
-      name: "initialize",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "retrieve",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "value",
-          type: "uint256",
-        },
-      ],
-      name: "store",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "x",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-  ];
-  const address = "0x02E5e3d63523982AD71B7866aDD7272FF6897eBB";
+  let Box;
+  let ownerBox;
+  let owner;
+  let addr1;
+  let addr2;
 
-  // Test case
-  it("retrieve a value less than 42 returns a value previously stored", async function () {
-    var provider = new ethers.JsonRpcProvider(url);
-    const box = new Contract(address, abi, provider);
-    const data = await fs.readFile(path.join(__dirname, "/keys/adminKey"));
-    const signer = new Wallet(data.toString(), provider);
-    const contractSigner = box.connect(signer);
-    await contractSigner.store(43);
-    expect((await box.retrieve()).toString()).to.equal("43");
+
+  before(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    console.log("owner:", owner.address);
+    console.log("addr1:", addr1.address);
+    console.log("addr2:", addr2.address);
+
+    Box = await ethers.getContractFactory("Box");
+    ownerBox = await upgrades.deployProxy(Box, [owner.address], {
+      initializer: "initialize", 
+    });
+    await ownerBox.waitForDeployment();
+    console.log("Contract deployed to address:", ownerBox.target);
+  });
+
+  it("should allow owner to get a value", async function () {
+    const newValue = 10;
+    const tx = await ownerBox.store(newValue);
+    await tx.wait();
+
+    expect(tx).to.emit(ownerBox, newValue).withArgs(owner.address, newValue);
+    const value = await ownerBox.retrieve();
+    console.log('value:', value);
+    expect(value).to.equal(newValue);
+  });
+
+  it("should disallow user1 to get a value", async function () {
+    const newValue = 20;
+    const tx = await ownerBox.store(newValue);
+    await tx.wait();
+    expect(tx).to.emit(ownerBox, newValue).withArgs(owner.address, newValue);
+    const value = await ownerBox.connect(owner).retrieve();
+    console.log('value:', value);
+    await ownerBox.connect(addr1).retrieve();
+    // expect(tx).to.emit(ownerBox, newValue).withArgs(owner.address, newValue);
+    // try {
+     
+    // } catch (error) {
+    //   console.log('error:', error);
+    //   expect(error).to.include();
+    // }
   });
 });
