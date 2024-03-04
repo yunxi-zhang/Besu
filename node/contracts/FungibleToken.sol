eaef163b8a2ffb310e87cbf4b9cd7699b84b8ad1
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-03-04 13:56:24 +0000

    update fungible token and its unit tests

--- node/contracts/FungibleToken.sol
+++ node/contracts/FungibleToken.sol
@@ -4,20 +4,41 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract FungibleToken is ERC20Upgradeable, AccessControlUpgradeable{
-
-    function initialize(string memory name, string memory symbol, address account, uint256 value) public initializer {
+contract FungibleToken is ERC20Upgradeable, AccessControlUpgradeable {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        address account,
+        uint256 value
+    ) public initializer {
         __ERC20_init(name, symbol);
         __AccessControl_init();
         _mint(account, value);
         _grantRole(DEFAULT_ADMIN_ROLE, account);
     }
 
-    function balanceOf(address account) public onlyRole(DEFAULT_ADMIN_ROLE) view virtual override returns (uint256) {
+    function balanceOf(
+        address account
+    )
+        public
+        view
+        virtual
+        override
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        returns (uint256)
+    {
         return super.balanceOf(account);
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 2;
+    }
+
+    function mint(address account, uint256 value) public {
+        super._mint(account, value);
+    }
+
+    function burn(address account, uint256 value) public {
+        super._burn(account, value);
     }
 }
--- node/test/FungibleToken.test.js
+++ node/test/FungibleToken.test.js
@@ -8,8 +8,10 @@ describe("FungibleToken", function () {
   const tokenSymbol = "*";
   const totalAmount = 1000;
   const transferAmount = 300;
+  const mintAmount = 100;
+  const burnAmount = 200;
 
-  before(async function () {
+  before(async () => {
     [owner, addr1] = await ethers.getSigners();
     Contract = await ethers.getContractFactory("FungibleToken");
     contractInstance = await upgrades.deployProxy(
@@ -23,25 +25,25 @@ describe("FungibleToken", function () {
     console.log("Contract deployed to address:", contractInstance.target);
   });
 
-  it("should get a token name", async function () {
+  it("should get a token name", async () => {
     const myTokenName = await contractInstance.name();
     console.log("myTokenName:", myTokenName);
     expect(myTokenName).to.equal(tokenName);
   });
 
-  it("should get a token symbol", async function () {
+  it("should get a token symbol", async () => {
     const myTokenSymbol = await contractInstance.symbol();
     console.log("myTokenSymbol:", myTokenSymbol);
     expect(myTokenSymbol).to.equal(tokenSymbol);
   });
 
-  it("should get an initial total supply", async function () {
+  it("should get an initial total supply", async () => {
     const myTokenTotalSupply = await contractInstance.totalSupply();
     console.log("myTokenTotalSupply:", myTokenTotalSupply);
     expect(myTokenTotalSupply).to.equal(totalAmount);
   });
 
-  it("should allow owner to get the remaining amount of the owner account", async function () {
+  it("should allow owner to get the remaining amount of the owner account", async () => {
     const tx = await contractInstance.transfer(addr1.address, transferAmount);
     await tx.wait();
 
@@ -50,13 +52,13 @@ describe("FungibleToken", function () {
     expect(ownerBalance).to.equal(totalAmount - transferAmount);
   });
 
-  it("should allow owner to get the remaining amount of the addr1 account", async function () {
+  it("should allow owner to get the remaining amount of the addr1 account", async () => {
     const addr1Balance = await contractInstance.balanceOf(addr1.address);
     console.log("addr1Balance:", addr1Balance);
     expect(addr1Balance).to.equal(transferAmount);
   });
 
-  it("should disallow addr1 to get the remaining amount", async function () {
+  it("should disallow addr1 to get the remaining amount", async () => {
     try {
       await contractInstance.connect(addr1).balanceOf(addr1.address);
     } catch (error) {
@@ -64,43 +66,77 @@ describe("FungibleToken", function () {
     }
   });
 
-  it("should get 0 when calling allowance by default", async function () {
-    const allowance = await contractInstance.allowance(owner.address, addr1.address);
-    console.log('allowance:', allowance);
+  it("should get 0 when calling allowance by default", async () => {
+    const allowance = await contractInstance.allowance(
+      owner.address,
+      addr1.address
+    );
+    console.log("allowance:", allowance);
     expect(allowance).to.equal(0);
   });
 
   it("should get 10 when calling allowance for addr1 on behalf of owner", async () => {
-    const allowance = 10; 
+    const allowance = 10;
     const tx1 = await contractInstance.approve(addr1.address, allowance);
     await tx1.wait();
 
-    const allowanceResult = await contractInstance.allowance(owner.address, addr1.address);
-    console.log('allowance result:', allowanceResult);
+    const allowanceResult = await contractInstance.allowance(
+      owner.address,
+      addr1.address
+    );
+    console.log("allowance result:", allowanceResult);
     expect(allowanceResult).to.equal(allowance);
-  })
+  });
 
   it("should get 10 when calling allowance for addr1 on behalf of owner again without resetting approve again", async () => {
-    const allowance = 10; 
-    const allowanceResult = await contractInstance.allowance(owner.address, addr1.address);
-    console.log('allowance result:', allowanceResult);
+    const allowance = 10;
+    const allowanceResult = await contractInstance.allowance(
+      owner.address,
+      addr1.address
+    );
+    console.log("allowance result:", allowanceResult);
     expect(allowanceResult).to.equal(allowance);
-  })
+  });
 
   it("should expect the right balance of each account and right allowance after the addr1 transfers the right amount on behalf of the owner", async () => {
-    const onBehalfTransferAmount = 10; 
-    const tx = await contractInstance.connect(addr1).transferFrom(owner.address, addr1.address, onBehalfTransferAmount);
+    const onBehalfTransferAmount = 10;
+    const tx = await contractInstance
+      .connect(addr1)
+      .transferFrom(owner.address, addr1.address, onBehalfTransferAmount);
     await tx.wait();
-    const allowanceResult = await contractInstance.allowance(owner.address, addr1.address);
-    console.log('allowance result:', allowanceResult);
+    const allowanceResult = await contractInstance.allowance(
+      owner.address,
+      addr1.address
+    );
+    console.log("allowance result:", allowanceResult);
     expect(allowanceResult).to.equal(0);
-    
+
     const ownerBalance = await contractInstance.balanceOf(owner.address);
     console.log("ownerBalance:", ownerBalance);
-    expect(ownerBalance).to.equal(totalAmount - transferAmount - onBehalfTransferAmount);
+    expect(ownerBalance).to.equal(
+      totalAmount - transferAmount - onBehalfTransferAmount
+    );
 
     const addr1Balance = await contractInstance.balanceOf(addr1.address);
     console.log("addr1Balance:", addr1Balance);
     expect(addr1Balance).to.equal(transferAmount + onBehalfTransferAmount);
-  })
+  });
+
+  it("should expect the total supply is increased after minting", async () => {
+    const tx = await contractInstance.mint(owner.address, mintAmount);
+    await tx.wait();
+
+    const myTokenTotalSupply = await contractInstance.totalSupply();
+    console.log("myTokenTotalSupply:", myTokenTotalSupply);
+    expect(myTokenTotalSupply).to.equal(totalAmount + mintAmount);
+  });
+
+  it("should expect the total supply is decreased after burning", async () => {
+    const tx = await contractInstance.burn(owner.address, burnAmount);
+    await tx.wait();
+
+    const myTokenTotalSupply = await contractInstance.totalSupply();
+    console.log("myTokenTotalSupply:", myTokenTotalSupply);
+    expect(myTokenTotalSupply).to.equal(totalAmount + mintAmount - burnAmount);
+  });
 });
