7e45d5f1f40b6de03241745e5396b12df4e94d7f
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-03-06 12:20:04 +0000

    update deploy upgradeable contract

--- node/scripts/deploy_upgradeable_contract.js
+++ node/scripts/deploy_upgradeable_contract.js
@@ -1,16 +1,33 @@
 const { ethers, upgrades } = require("hardhat");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
+const { time } = require("console");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
   const Box = await ethers.getContractFactory("Box");
-  console.log('Deploying Box...');
-  const box = await upgrades.deployProxy(Box, [45], { initializer: 'store' });
+  console.log("Deploying Box...");
+  const box = await upgrades.deployProxy(Box, [deployer.address], {
+    initializer: "initialize",
+  });
   await box.waitForDeployment();
-  console.log('Box deployed to:', box.target);
+  console.log("Box deployed to:", box.target);
   const logDir = path.join(__dirname, "../contractLogs/");
-  const writer = fs.createWriteStream(logDir + '/deployContractLog.json');
-  writer.write('{"contract":"Box", "address":"' + box.target + '", "timeStamp":"' + Date.now() + '", "humanReadableTimeStamp":"' + new Date(Date.now() * 1000) + '"}')
+  const writer = fs.createWriteStream(logDir + "/deployContractLog.json");
+  const timeStamp = await Date.now();
+  const date = await new Date(timeStamp);
+  console.log("timeStamp:", timeStamp);
+  console.log("date:", date);
+  await writer.write(
+    '{"contract":"Box", "address":"' +
+      box.target +
+      '", "timeStamp":"' +
+      timeStamp +
+      '", "humanReadableTimeStamp":"' +
+      date +
+      '"}'
+  );
 }
 
 main();
