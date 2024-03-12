267008f1ae40ea1701bfdf9588e6322f4d24a9e5
yunxi.zhang <yunxi.zhang@hotmail.com>
2024-03-12 14:42:30 +0000

    update upgrade script by supporting multiple contracts upgrade

--- node/scripts/upgrade.js
+++ node/scripts/upgrade.js
@@ -6,19 +6,38 @@ const path = require("path");
 async function main() {
   const logDir = path.join(__dirname, "../contractLogs/");
   const contracts = JSON.parse(
-    fs.readFileSync(logDir + "/deployContractLog.json", "utf-8")
+    fs.readFileSync(logDir + "/contractDeploymentLog.json", "utf-8")
   );
-  console.log("contract:", contracts.contract);
-  const contract = await ethers.getContractFactory(contracts.contract);
-  console.log(
-    "Upgrading contract",
-    contracts.contract,
-    "at address:",
-    contracts.address,
-    "..."
-  );
-  await upgrades.upgradeProxy(contracts.address, contract);
-  console.log("Contract", contracts.contract, "upgraded");
+
+  const promises = contracts.map(async (contractObject) => {
+    console.log(
+      "Upgrading contract",
+      contractObject.contract,
+      "at address:",
+      contractObject.address,
+      "..."
+    );
+    const contract = await ethers.getContractFactory(contractObject.contract);
+    return await upgrades.upgradeProxy(contractObject.address, contract);
+  });
+
+  const contractUpgradeResult = await Promise.all(promises);
+  contractUpgradeResult.forEach((result) => {
+    if (result.target) {
+      for (i = 0; i < contracts.length; i++) {
+        if (contracts[i].address == result.target) {
+          console.log(
+            "Contract",
+            contracts[i].contract,
+            "upgraded successfully"
+          );
+          break;
+        }
+      }
+    } else {
+      console.log("There're failures in contract upgrade, please check");
+    }
+  });
 }
 
 main();
